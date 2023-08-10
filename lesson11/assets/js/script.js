972618d8ada12054dd3e19d512b07294f5ec168c
Narmin <mnermin064@gmail.com>
2023-08-10 12:51:47 +0400

    Js enter

---
+++ lesson11/assets/js/script.js
@@ -0,0 +1,68 @@
+console.log('External javascript');
+console.log('Hello');
+
+// Mesaj gondermek
+
+// alert('Salam')
+
+// Istifadeciden cavab almaq meqsedile
+
+// prompt("Nece yasin var")
+
+
+// 3 yere
+// 1.Primitive - besit, sade
+// 2.Non - primitive - murekkeb
+// 3.Xususi - trivial
+
+
+// Data tipleri
+
+// 1.Number
+
+var a = 5;
+console.log("A deyiskeninin deyeri " + a);
+
+// Sehifeye yazdirmaq
+document.write(a)
+
+
+// 2.String
+
+
+var b = "Narmin"
+console.log("Menim adim: " + b);
+
+// 3.Boolean 
+
+var c = true
+var d = false;
+
+console.log(c);
+console.log(d);
+
+
+// Trivial data type 
+
+// 4.Null
+
+var e = null;
+console.log(e);
+
+
+// 5.Undefined
+
+var m ;
+console.log(m);
+
+
+
+
+
+
+
+
+
+var number1 = 12
+var number2 = 20
+console.log(number1 + number2);
