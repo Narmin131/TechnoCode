4795b861300c2f4c2d9e5ef6465f81fdd6883d37
Narmin <mnermin064@gmail.com>
2023-08-16 17:17:34 +0400

    Header link

---
+++ lesson13/assets/js/script.js
@@ -0,0 +1,69 @@
+console.log("External javascript");
+console.log("Hello");
+
+// Mesaj gondermek
+
+// alert('Salam')
+
+// Istifadeciden cavab almaq meqsedile
+
+// prompt("Nece yasin var")
+
+// 3 yere
+// 1.Primitive - besit, sade
+// 2.Non - primitive - murekkeb
+// 3.Xususi - trivial
+
+// Data tipleri
+
+// 1.Number
+
+var a = 5;
+console.log("A deyiskeninin deyeri " + a);
+
+// Sehifeye yazdirmaq
+document.write(a);
+
+// 2.String
+
+var b = "Narmin";
+console.log("Menim adim: " + b);
+
+// 3.Boolean
+
+var c = true;
+var d = false;
+
+console.log(c);
+console.log(d);
+
+// Trivial data type
+
+// 4.Null
+
+var e = null;
+console.log(e);
+
+// 5.Undefined
+
+var m;
+console.log(m);
+
+// Non-primitive - murekkeb
+
+// 6.Array
+
+var myArray = [12, "hello", true, 34, null, "hi"];
+
+// 7.Objecy
+
+var myObject = {
+  name: "Narmin",
+  age: 21,
+};
+
+
+
+var number1 = 12;
+var number2 = 20;
+console.log(number1 + number2);
